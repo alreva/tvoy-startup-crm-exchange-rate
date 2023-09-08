c016bd24e8f54eae3ef9fe9d65f8eb26e8dc05bb
Alex Reva <alreva@gmail.com>
2023-09-08 18:19:33 +0300

    added fee calculation

--- src/content.js
+++ src/content.js
@@ -4,11 +4,8 @@
     }
     
     log('Hello :)');
-    
-    async function fetchCurrencyRate() {
-    
-        log('fetching currency exchange...');
-    
+
+    function getInvoiceDate() {
         const invoiceDateStr = document.getElementById('sales_invoice_proforma_form_soldAt').value ?? '';
         if (!invoiceDateStr) {
             return;
@@ -18,11 +15,18 @@
         log('invoice date: ' + invoiceDateStr);
         const [yr, mon, day] = invoiceDateStr.split('-');
         const dt = new Date(yr, mon - 1, day);
+        return dt;
+    }
+
+    async function fetchCurrencyRate(invoiceDate) {
+
+        log('fetching currency exchange rate...');
 
         // Subtracting one day from the invoice date to fetch the exchange rates
         // available until the day before the invoice date. This ensures that the
         // rate used corresponds to the latest available information prior to the
         // invoice date.
+        var dt = new Date(invoiceDate);
         dt.setDate(dt.getDate() - 1);
 
         const data = await chrome.runtime.sendMessage({
@@ -58,16 +62,82 @@
     
         const rate = rates[0].mid;
         log('exchange rate: ' + rate);
-        
+
+        return parseFloat(rate);
+    }
+    
+    async function setCurrencyRate() {
+        const dt = getInvoiceDate();
+        const rate = await fetchCurrencyRate(dt);
         document.getElementById('sales_invoice_proforma_form_payment_exchange').value = rate;
+        return rate;
+    }
+
+    async function ensureCurrencyRate() {
+        let rate = document.getElementById('sales_invoice_proforma_form_payment_exchange').value;
+        return rate ? parseFloat(rate) : await setCurrencyRate();
+    }
+
+    async function calcFoundationFee() {
+        log('calculating PLN fee from USD exchange rate...');
+
+        let errAnalysisIdx = -1
+        
+        for (let i = 0; i < 100; i++) {
+
+            let errorAnalysisElm = document.getElementById(`sales_invoice_proforma_form_products_${i}_item_product`);
+
+            if (!errorAnalysisElm) {
+                continue;
+            }
+
+            if (!errorAnalysisElm.value?.toLowerCase()?.startsWith('error analysis')) {
+                continue;
+            }
+
+            log(`Error Analysis line found. Index: ${i}`);
+            errAnalysisIdx = i;
+            break;
+        }
+
+        if (errAnalysisIdx < 0) {
+            log('Error Analysis line not found. Skipping fee calculation.');
+            return;
+        }
+
+        const forceRefreshCurrencyRate = true;
+        const plnFeeAmount = 300.0;
+        const rate = forceRefreshCurrencyRate ? await setCurrencyRate() : await ensureCurrencyRate();
+
+        log(`Calculating a ${plnFeeAmount} PLN fee in USD using the rate of ${rate} PLN per USD...`)
+
+        const usdFeeAmount = plnFeeAmount / rate;
+        const usdFeeAmountRounded = usdFeeAmount.toFixed(2);
+
+        log(`The fee is ${usdFeeAmount} USD rounded to ${usdFeeAmountRounded} USD`);
+
+        document.getElementById(`sales_invoice_proforma_form_products_${errAnalysisIdx}_unitPrice`).value = usdFeeAmountRounded;
     }
     
     $('<button>', {
         text: 'fetch',
     })
         .on('click', (evt) => {
-            fetchCurrencyRate();
+            setCurrencyRate();
             evt.preventDefault();
         })
         .insertAfter('#sales_invoice_proforma_form_payment_exchange');
+
+    $('<button>', {
+        text: 'Calculate Fee'
+    })
+        .addClass('btn btn-success')
+        .on('click', (evt) => {
+            calcFoundationFee();
+            evt.preventDefault();
+        })
+        .insertAfter('#proformaItemServicesAdd');
+
+    $('#proformaItemServicesAdd').css('margin-right', '4px');
+
 })();
